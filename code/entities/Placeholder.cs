0c00a009fcc10d0463d198a072145dca2c0b02e6
Conna Wiles <kurozael@gmail.com>
2021-07-02 16:08:37 +0100

    Added placeholder boxes so people know these buildings models are temporary

---
+++ code/entities/Placeholder.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+
+namespace Facepunch.RTS
+{
+	public partial class Placeholder : RenderEntity
+	{
+		public Material Material = Material.Load( "materials/placeholder.vmat" );
+		public Color Color { get; set; }
+		public float Alpha { get; set; } = 1f;
+
+		public override void DoRender( SceneObject sceneObject  )
+		{
+			if ( !EnableDrawing ) return;
+
+			var vertexBuffer = Render.GetDynamicVB( true );
+
+			vertexBuffer.AddCube( Position, RenderBounds.Size, Rotation );
+
+			Render.Set( "TintColor", Color );
+			Render.Set( "Translucency", Alpha );
+
+			vertexBuffer.Draw( Material );
+		}
+	}
+}
