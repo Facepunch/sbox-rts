342a89c83e9055249646d928facb67261c651e1c
Conna Wiles <kurozael@gmail.com>
2021-07-14 02:01:45 +0100

    Give the buggy a smaller node size

--- code/items/units/Buggy.cs
+++ code/items/units/Buggy.cs
@@ -16,7 +16,7 @@ namespace Facepunch.RTS.Units
 		public override bool UseRenderColor => true;
 		public override float RotateToTargetSpeed => 10f;
 		public override string Entity => "unit_buggy";
-		public override int NodeSize => 200;
+		public override int NodeSize => 100;
 		public override float AttackRadius => 1000f;
 		public override float LineOfSightRadius => 1000f;
 		public override OccupiableSettings Occupiable => new()
