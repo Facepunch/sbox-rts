2c3ab23f1e7e9114792b52bf14714a39f6d7f256
Conna Wiles <kurozael@gmail.com>
2021-07-28 01:38:32 +0100

    Use CP#2.x even for the health, oops!

--- code/entities/abilities/DomeShieldEntity.cs
+++ code/entities/abilities/DomeShieldEntity.cs
@@ -106,7 +106,7 @@ namespace Facepunch.RTS
 			if ( Effect != null )
 			{
 				Effect.SetPosition( 0, Position );
-				Effect.SetPosition( 1, new Vector3( 100f - ((Health / MaxHealth) * 100f), 0, 0f ) );
+				Effect.SetPosition( 2, new Vector3( 100f - ((Health / MaxHealth) * 100f), 0, 0f ) );
 			}
 		}
 	}
