f964a1222f3c5d1685e3a9e159abf338702e74cd
bakscratch <lt_8@hotmail.co.uk>
2021-08-12 10:25:43 +0100

    Day night audio starts as soon as the player joins

--- code/gamelib/daynight/entities/DayNightAudio.cs
+++ code/gamelib/daynight/entities/DayNightAudio.cs
@@ -6,7 +6,7 @@ namespace Gamelib.DayNight
 {
 	[Library( "snd_daynight" )]
 	[Hammer.EditorSprite("editor/snd_daynight.vmat")]
-	public partial class DayNightAudio : ModelEntity
+	public partial class DayNightAudio : Entity
 	{
 		[Property(	FGDType = "sound", Title = "Sound For Day" )]
 		public string DayAmbient { get; set; }
@@ -25,23 +25,42 @@ namespace Gamelib.DayNight
 		private Sound DaySound { get; set; }
 
 
+
 		public override void Spawn()
 		{
 			base.Spawn();
-		
 			DayNightManager.OnSectionChanged += HandleSectionChanged;
+
+			_ = PlayDaySoundAsync( 2f );
 		}
 
-		//public override void ClientSpawn()
-		//{
-		//	base.ClientSpawn();
+		public override void ClientSpawn()
+		{
+			base.ClientSpawn();
 
-		//	DayNightManager.OnSectionChanged += HandleSectionChanged;
-		//}
+		}
 
+		private TimeSection CurrentSection;
+
+		public async Task PlayDaySoundAsync( float delay = 0 )
+		{
+			await GameTask.DelaySeconds( delay );
+
+			if ( DayNightManager.TimeOfDay > 0f && DayNightManager.TimeOfDay <= 18f )
+			{
+				DaySound = PlaySound( DayAmbient );
+			}
+			else if ( DayNightManager.TimeOfDay > 18f && DayNightManager.TimeOfDay <= 24f )
+			{
+				NightSound = PlaySound( NightAmbient );
+			}
+
+		}
 
 		private void HandleSectionChanged( TimeSection section )
 		{
+			CurrentSection = section;
+
 			if ( section == TimeSection.Dawn )
 			{
 				NightSound.Stop();
@@ -49,6 +68,7 @@ namespace Gamelib.DayNight
 
 				PlaySound( DayStart );
 			}
+
 			else if ( section == TimeSection.Dusk )
 			{
 				DaySound.Stop();
