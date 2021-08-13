94cbe247f902000096cd031e4db6c165e4814061
Conna Wiles <kurozael@gmail.com>
2021-08-13 13:38:07 +0100

    Attack Drone fires much faster but has less damage

--- code/entities/weapons/AttackDroneWeapon.cs
+++ code/entities/weapons/AttackDroneWeapon.cs
@@ -5,18 +5,11 @@ namespace Facepunch.RTS
 	[Library("weapon_attack_drone")]
 	public partial class AttackDroneWeapon : Weapon
 	{
-		public override float FireRate => 0.3f;
-		public override int BaseDamage => 8;
+		public override float FireRate => 0.1f;
+		public override int BaseDamage => 4;
 		public override bool BoneMerge => false;
-		public override string MuzzleFlash => "particles/weapons/muzzle_flash/muzzle_large/muzzleflash_large.vpcf";
 		public override string SoundName => "rts.smg.shoot";
 		public override float Force => 5f;
-
-		public override void Attack()
-		{
-
-			base.Attack();
-		}
 
 		public override Transform? GetMuzzle()
 		{
