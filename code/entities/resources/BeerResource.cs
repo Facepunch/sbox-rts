e7f821624a1904610de2ac7ee223a76266fea2b4
Conna Wiles <kurozael@gmail.com>
2021-08-19 12:29:46 +0100

    Added a different sound for gathering beer from barrels to mining rocks

--- code/entities/resources/BeerResource.cs
+++ code/entities/resources/BeerResource.cs
@@ -8,6 +8,10 @@ namespace Facepunch.RTS
 	public partial class BeerResource : ResourceEntity
 	{
 		public override ResourceType Resource => ResourceType.Beer;
+		public override string[] GatherSounds => new string[]
+		{
+			"gatherbeer1"
+		};
 		public override string Description => "You can find Beer for your empire in this cache.";
 		public override string Name => "Beer Cache";
 	}
