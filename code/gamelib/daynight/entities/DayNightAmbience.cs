dcfe022e9fa25f9fbf56a5ee254ebeae5a13731f
Conna Wiles <kurozael@gmail.com>
2021-09-08 09:13:32 +0100

    Stashing here

--- code/gamelib/daynight/entities/DayNightAmbience.cs
+++ code/gamelib/daynight/entities/DayNightAmbience.cs
@@ -1,13 +1,13 @@
-﻿using Sandbox;
+﻿using Facepunch.RTS;
+using Sandbox;
+using System;
 
 namespace Gamelib.DayNight
 {
-
 	/// <summary>
 	/// An audio controller for when a looping sound should play at each point in the day.
 	/// </summary>
 	/// 
-
 	[Library( "daynight_ambience" )]
 	[Hammer.EntityTool( "Day Night Ambience", "Day Night System" )]
 	[Hammer.EditorSprite("editor/snd_daynight.vmat")]
@@ -22,35 +22,96 @@ namespace Gamelib.DayNight
 		[Property(	FGDType = "sound", Title = "Night Ambient Sound" )]
 		public string NightAmbience { get; set; }
 
+		public class Transition
+		{
+			public Sound From { get; set; }
+			public Sound To { get; set; }
+
+			public RealTimeUntil EndTime { get; private set; }
+			public float Progress { get; private set; }
+			public float Duration { get; private set; }
+			public bool IsComplete { get; private set; }
+
+			public Transition()
+			{
+				IsComplete = true;
+			}
+
+			public void Start( Sound from, Sound to, float duration )
+			{
+				From.Stop();
+
+				IsComplete = false;
+				Progress = 0f;
+				Duration = duration;
+				EndTime = duration;
+				From = from;
+				To = to;
+
+				Update();
+			}
+
+			public void Update()
+			{
+				if ( IsComplete ) return;
+
+				Progress = Math.Clamp( 1f - (EndTime / Duration), 0f, 1f );
+
+				From.SetVolume( 1f - Progress );
+				To.SetVolume( Progress );
+
+				if ( Progress >= 1f )
+				{
+					IsComplete = true;
+					From.Stop();
+				}
+			}
+		}
+
+		private Transition SoundTransition { get; set; }
 		private Sound CurrentSound { get; set; }
 
 		public override void Spawn()
 		{
-			base.Spawn();
-
 			DayNightManager.OnSectionChanged += HandleSectionChanged;
+			SoundTransition = new Transition();
+
+			base.Spawn();
 		}
 
 		private void HandleSectionChanged( TimeSection section )
 		{
-			CurrentSound.Stop();
+			if ( IsClient ) return;
 
 			if ( section == TimeSection.Dawn )
 			{
-				CurrentSound = PlaySound( DawnAmbience );
+				TransitionTo( DawnAmbience );
 			}
 			else if ( section == TimeSection.Day )
 			{
-				CurrentSound = PlaySound( DayAmbience );
+				TransitionTo( DayAmbience );
 			}
 			else if ( section == TimeSection.Dusk )
 			{
-				CurrentSound = PlaySound( DuskAmbience );
+				TransitionTo( DuskAmbience );
 			}
 			else if ( section == TimeSection.Night )
 			{
-				CurrentSound = PlaySound( NightAmbience );
+				TransitionTo( NightAmbience );
 			}
+		}
+
+		private void TransitionTo( string soundName )
+		{
+			var sound = Sound.FromScreen( soundName );
+			SoundTransition.Start( CurrentSound, sound, 5f );
+			CurrentSound = sound;
+		}
+
+		[Event.Tick.Server]
+		private void ServerTick()
+		{
+			SoundTransition?.Update();
 		}
 	}
 }
