79d83887fc7bf3f11b30e16fdeb98a4f23257220
kurozael <kurozael@gmail.com>
2022-03-14 19:09:05 +0000

    Updated description of Basic Ballistics to emphasize that it affects ANY projectile type

--- code/items/tech/BasicBallistics.cs
+++ code/items/tech/BasicBallistics.cs
@@ -9,7 +9,7 @@ namespace Facepunch.RTS.Tech
 	{
 		public override string Name => "Basic Ballistics";
 		public override string UniqueId => "tech.basicballistics";
-		public override string Description => "Increases the attack power of projectiles by +1.";
+		public override string Description => "Increases the attack power of any projectile type by +1.";
 		public override Texture Icon => Texture.Load( FileSystem.Mounted, "ui/icons/wheels.png" );
 		public override int BuildTime => 60;
 		public override Dictionary<ResourceType, int> Costs => new()
